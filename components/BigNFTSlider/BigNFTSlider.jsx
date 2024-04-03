676214096330186e6f4ad0d1809fffa905ff1390
VU QUOC HUNG <20521378@gm.uit.edu.vn>
2024-04-03 15:17:17 +0700

    Create Dynamic Slider Component

---
+++ components/BigNFTSlider/BigNFTSlider.jsx
@@ -0,0 +1,217 @@
+import React, { useState, useEffect, useCallback } from "react";
+import Image from "next/image";
+import { AiFillFire, AiFillHeart, AiOutlineHeart } from "react-icons/ai";
+import { MdVerified, MdTimer } from "react-icons/md";
+import { TbArrowBigLeftLines, TbArrowBigRightLine } from "react-icons/tb";
+
+//INTERNAL IMPORT
+import Style from "./bigNFTSlider.module.css";
+import images from "../../img";
+import Button from "../Button/Button";
+
+const BigNFTSlider = () => {
+  const [idNumber, setIdNumber] = useState(1);
+
+  const sliderData = [
+    {
+      title: "Hello NFT",
+      id: 1,
+      name: "Vu Quoc Hung",
+      collection: "Gym",
+      price: "00000064664 ETH",
+      like: 243,
+      image: images.user1,
+      nftImage: images.nft_image_1,
+      time: {
+        days: 27,
+        hours: 10,
+        minutes: 11,
+        seconds: 35,
+      },
+    },
+    {
+      title: "Buddy NFT",
+      id: 2,
+      name: "Yana Müjde",
+      collection: "Home",
+      price: "00000064664 ETH",
+      like: 243,
+      image: images.user2,
+      nftImage: images.nft_image_2,
+      time: {
+        days: 27,
+        hours: 10,
+        minutes: 11,
+        seconds: 35,
+      },
+    },
+    {
+      title: "Gym NFT",
+      id: 3,
+      name: "Sassa Ségdae",
+      collection: "Gym",
+      price: "00000064664 ETH",
+      like: 243,
+      image: images.user3,
+      nftImage: images.nft_image_3,
+      time: {
+        days: 27,
+        hours: 10,
+        minutes: 11,
+        seconds: 35,
+      },
+    },
+    {
+      title: "Home NFT",
+      id: 4,
+      name: "Sassa Ségdae",
+      collection: "Gym",
+      price: "00000064664 ETH",
+      like: 243,
+      image: images.user4,
+      nftImage: images.nft_image_1,
+      time: {
+        days: 27,
+        hours: 10,
+        minutes: 11,
+        seconds: 35,
+      },
+    },
+  ];
+
+  //--------INC
+  const inc = useCallback(() => {
+    if (idNumber + 1 < sliderData.length) {
+      setIdNumber((pre) => pre + 1);
+    }
+  }, [idNumber, sliderData.length]);
+
+  //---------DEC
+  const dec = useCallback(() => {
+    if (idNumber > 0) {
+      setIdNumber((pre) => pre - 1);
+    }
+  }, [idNumber]);
+
+  return (
+    <div className={Style.bigNFTSlider}>
+      <div className={Style.bigNFTSlider_box}>
+        <div className={Style.bigNFTSlider_box_left}>
+          <h2>{sliderData[idNumber].title}</h2>
+          <div className={Style.bigNFTSlider_box_left_creator}>
+            <div className={Style.bigNFTSlider_box_left_creator_profile}>
+              <Image
+                className={Style.bigNFTSlider_box_left_creator_profile_img}
+                src={sliderData[idNumber].image}
+                alt="profile image"
+                width={50}
+                height={50}
+              />
+              <div className={Style.bigNFTSlider_box_left_creator_profile_info}>
+                <p>Người Sáng Tạo</p>
+                <h4>
+                  {sliderData[idNumber].name}
+                  <span>
+                    <MdVerified />
+                  </span>
+                </h4>
+              </div>
+            </div>
+
+            <div className={Style.bigNFTSlider_box_left_creator_collection}>
+              <AiFillFire
+                className={Style.bigNFTSlider_box_left_creator_collection_icon}
+              />
+
+              <div
+                className={Style.bigNFTSlider_box_left_creator_collection_info}
+              >
+                <p>Bộ Sưu Tập</p>
+                <h4>{sliderData[idNumber].collection}</h4>
+              </div>
+            </div>
+          </div>
+
+          <div className={Style.bigNFTSlider_box_left_bidding}>
+            <div className={Style.bigNFTSlider_box_left_bidding_box}>
+              <small>Giá Thầu Hiện Tại</small>
+              <p>
+                {sliderData[idNumber].price} <spna>$221,21</spna>
+              </p>
+            </div>
+
+            <p className={Style.bigNFTSlider_box_left_bidding_box_auction}>
+              <MdTimer
+                className={Style.bigNFTSlider_box_left_bidding_box_icon}
+              />
+              <span>Phiên đấu giá kết thúc vào</span>
+            </p>
+
+            <div className={Style.bigNFTSlider_box_left_bidding_box_timer}>
+              <div
+                className={Style.bigNFTSlider_box_left_bidding_box_timer_item}
+              >
+                <p>{sliderData[idNumber].time.days}</p>
+                <span>Ngày</span>
+              </div>
+
+              <div
+                className={Style.bigNFTSlider_box_left_bidding_box_timer_item}
+              >
+                <p>{sliderData[idNumber].time.hours}</p>
+                <span>Giờ</span>
+              </div>
+
+              <div
+                className={Style.bigNFTSlider_box_left_bidding_box_timer_item}
+              >
+                <p>{sliderData[idNumber].time.minutes}</p>
+                <span>Phút</span>
+              </div>
+
+              <div
+                className={Style.bigNFTSlider_box_left_bidding_box_timer_item}
+              >
+                <p>{sliderData[idNumber].time.seconds}</p>
+                <span>Giây</span>
+              </div>
+            </div>
+
+            <div className={Style.bigNFTSlider_box_left_button}>
+              <Button btnName="Đặt" handleClick={() => {}} />
+              <Button btnName="Xem" handleClick={() => {}} />
+            </div>
+          </div>
+
+          <div className={Style.bigNFTSlider_box_left_sliderBtn}>
+            <TbArrowBigLeftLines
+              className={Style.bigNFTSlider_box_left_sliderBtn_icon}
+              onClick={() => dec()}
+            />
+            <TbArrowBigRightLine
+              className={Style.bigNFTSlider_box_left_sliderBtn_icon}
+              onClick={() => inc()}
+            />
+          </div>
+        </div>
+
+        <div className={Style.bigNFTSlider_box_right}>
+          <div className={Style.bigNFTSlider_box_right_box}>
+            <Image
+              src={sliderData[idNumber].nftImage}
+              alt="NFT IMAGE"
+              className={Style.bigNFTSlider_box_right_box_img}
+            />
+
+            <div className={Style.bigNFTSlider_box_right_box_like}>
+              <AiFillHeart />
+              <span>{sliderData[idNumber].like}</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default BigNFTSlider;
