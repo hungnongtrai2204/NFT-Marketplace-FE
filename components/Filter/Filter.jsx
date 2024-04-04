ae851b7e0a56579966deaa77fa88c60dbbf71afc
Vu Quoc Hung <92137335+hungnongtrai2204@users.noreply.github.com>
2024-04-04 21:46:18 +0700

    Create Filter Section

---
+++ components/Filter/Filter.jsx
@@ -0,0 +1,131 @@
+import React, { useState } from "react";
+import {
+  FaFilter,
+  FaAngleDown,
+  FaAngleUp,
+  FaWallet,
+  FaMusic,
+  FaVideo,
+  FaImages,
+  FaUserAlt,
+} from "react-icons/fa";
+import { AiFillCloseCircle } from "react-icons/ai";
+import { MdVerified } from "react-icons/md";
+import { TiTick } from "react-icons/ti";
+
+//INTERNAL IMPORT
+import Style from "./Filter.module.css";
+
+const Filter = () => {
+  const [filter, setFilter] = useState(true);
+  const [image, setImage] = useState(true);
+  const [video, setVideo] = useState(true);
+  const [music, setMusic] = useState(true);
+
+  //FUNCTION SECTION
+  const openFilter = () => {
+    if (!filter) {
+      setFilter(true);
+    } else {
+      setFilter(false);
+    }
+  };
+
+  const openImage = () => {
+    if (!image) {
+      setImage(true);
+    } else {
+      setImage(false);
+    }
+  };
+
+  const openVideo = () => {
+    if (!video) {
+      setVideo(true);
+    } else {
+      setVideo(false);
+    }
+  };
+
+  const openMusic = () => {
+    if (!music) {
+      setMusic(true);
+    } else {
+      setMusic(false);
+    }
+  };
+
+  return (
+    <div className={Style.filter}>
+      <div className={Style.filter_box}>
+        <div className={Style.filter_box_left}>
+          <button onClick={() => {}}>NFTs</button>
+          <button onClick={() => {}}>Nghệ Thuật</button>
+          <button onClick={() => {}}>Âm Nhạc</button>
+          <button onClick={() => {}}>Thể Thao</button>
+          <button onClick={() => {}}>Nhiếp Ảnh</button>
+        </div>
+
+        <div className={Style.filter_box_right}>
+          <div
+            className={Style.filter_box_right_box}
+            onClick={() => openFilter()}
+          >
+            <FaFilter />
+            <span>Lọc</span> {filter ? <FaAngleDown /> : <FaAngleUp />}
+          </div>
+        </div>
+      </div>
+
+      {filter && (
+        <div className={Style.filter_box_items}>
+          <div className={Style.filter_box_items_box}>
+            <div className={Style.filter_box_items_box_item}>
+              <FaWallet /> <span>10 ETH</span>
+              <AiFillCloseCircle />
+            </div>
+          </div>
+
+          <div className={Style.filter_box_items_box}>
+            <div
+              className={Style.filter_box_items_box_item_trans}
+              onClick={() => openImage()}
+            >
+              <FaImages /> <small>Hình Ảnh</small>
+              {image ? <AiFillCloseCircle /> : <TiTick />}
+            </div>
+          </div>
+
+          <div className={Style.filter_box_items_box}>
+            <div
+              className={Style.filter_box_items_box_item_trans}
+              onClick={() => openVideo()}
+            >
+              <FaVideo /> <small>Videos</small>
+              {video ? <AiFillCloseCircle /> : <TiTick />}
+            </div>
+          </div>
+
+          <div className={Style.filter_box_items_box}>
+            <div
+              className={Style.filter_box_items_box_item_trans}
+              onClick={() => openMusic()}
+            >
+              <FaMusic /> <small>Âm Nhạc</small>
+              {music ? <AiFillCloseCircle /> : <TiTick />}
+            </div>
+          </div>
+
+          <div className={Style.filter_box_items_box}>
+            <div className={Style.filter_box_items_box_item}>
+              <FaUserAlt /> <span>Đã Xác Minh</span>
+              <MdVerified />
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Filter;
--- pages/index.js
+++ pages/index.js
@@ -9,6 +9,7 @@ import {
   Subscribe,
   Title,
   Category,
+  Filter,
 } from "../components/componentsindex";
 
 const Home = () => {
@@ -17,6 +18,11 @@ const Home = () => {
       <HeroSection />
       <Service />
       <BigNFTSilder />
+      <Title
+        heading="NFT Nổi Bật"
+        paragraph="Khám phá những NFT xuất sắc nhất trong mọi lĩnh vực của cuộc sống."
+      />
+      <Filter />
       <Title
         heading="Duyệt theo chủ đề"
         paragraph="Khám phá NFT trong các danh mục được yêu thích nhất."
